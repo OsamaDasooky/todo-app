905e7d4a74070a65ccf028dfb431298e78bac906
OsamaDasooky <osamadasooky6@gmail.com>
2022-08-10 17:16:23 +0300

    submit the task

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let name = prompt("What is your name?", "");
+
+let gender  = prompt("Please enter your gender.", "");
+
+let age = prompt('How old are you?', "");
+if (age == 0) {
+    alert(`There is no such thing as zero age! Please enter another value.`);
+    age =prompt('How old are you?', "");
+}
+
+let question = confirm("Do you want to go the welcoming message?");
+
+if (question) {
+    if (gender === "Male" || gender === "male") {
+        alert(`welcome Mr.${name} , your age is ${age}`);
+    } else if (gender === "Female" || gender === "female") {
+        alert(`welcome Ms.${name} , your age is ${age}`);
+    }else {
+        alert(`welcome ${name} , your age is ${age}`);
+    }
+}
+
+
+
