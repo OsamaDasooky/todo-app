4525801d9bf6484b89dee240a89009031a79965d
OsamaDasooky <osamadasooky6@gmail.com>
2022-08-11 23:05:03 +0300

    edit of input

--- app.js
+++ app.js
@@ -24,50 +24,37 @@ if (question) {
 let theAnss = [];
 let skepQus = true;
 let doYou1 = prompt('Do you love pizza ?', "");
-while (skepQus) {
     if ((doYou1 === "y") || (doYou1 === "yes") || (doYou1 === "Yes")
         || (doYou1 === "n") || (doYou1 === "no") || (doYou1 === "No")) {
         theAnss.push(doYou1);
-        break;
-    } else if (doYou1 === "") {
+    } else  {
         theAnss.push("invalid");
-        break;
     }
-    doYou1 = prompt(`please enter yes or no
-    Do you love pizza ?`, "");
     
-}  
+  
 //==========================================================
 let doYou2 = prompt(`Do you love travel ?`, "");
-while (skepQus) {
+
     if ((doYou2 === "y") || (doYou2 === "yes") || (doYou2 === "Yes")
         || (doYou2 === "n") || (doYou2 === "no") || (doYou2 === "No")) {
         theAnss.push(doYou2);
-        break;
-    } else if (doYou2 === "") {
+    } else  {
         theAnss.push("invalid");
-        break;
     }
-    doYou2 = prompt(`please enter yes or no 
-    Do you  love travel ?`, "");
     
-} 
+
 //==========================================================
 
 let doYou3 = prompt('Do you love Orange Academy?', "");
-while (skepQus) {
+
     if ((doYou3 === "y") || (doYou3 === "yes") || (doYou3 === "Yes")
         || (doYou3 === "n") || (doYou3 === "no") || (doYou3 === "No")) {
         theAnss.push(doYou3);
-        break;
-    } else if (doYou3 === "") {
+    } else {
         theAnss.push("invalid");
-        break;
     }
-    doYou3 = prompt(`please enter yes or no 
-    Do you  love Orange Academy ?`, "");
     
-} 
+
 //==========================================================
 let ratStar = prompt('how many stars they rate your website?', "");
 if (ratStar < 0 ) {     
